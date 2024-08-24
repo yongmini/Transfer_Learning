861486964818bfc6cbbe38ac8051ed3390c5d6b6
yongmini <89248050+yongmini@users.noreply.github.com>
2024-08-24 14:24:28 +0000

    t

--- code/DeepDA/data_loader.py
+++ code/DeepDA/data_loader.py
@@ -20,29 +20,31 @@ class LimitedImageFolder(datasets.ImageFolder):
                 class_count[class_idx] += 1
         
         return limited_samples
-
+           #  transforms.CenterCrop(224),
 def load_data(data_folder, batch_size, train, num_workers=0, **kwargs):
     transform = {
         'train': transforms.Compose(
-            [transforms.Resize([256, 256]),
+            [transforms.CenterCrop(512),
                 transforms.RandomCrop(224),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                   std=[0.229, 0.224, 0.225])]),
         'test': transforms.Compose(
-            [transforms.Resize([224, 224]),
+            [transforms.CenterCrop(224),
+                transforms.Resize([224, 224]),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                   std=[0.229, 0.224, 0.225])])
     }
 
-   # data = LimitedImageFolder(root=data_folder, transform=transform['train' if train else 'test'], max_images_per_class=10)
+  #  data = LimitedImageFolder(root=data_folder, transform=transform['train' if train else 'test'], max_images_per_class=10)
     data = datasets.ImageFolder(root=data_folder, transform=transform['train' if train else 'test'])
     data_loader = get_data_loader(data, batch_size=batch_size, 
                                 shuffle=True if train else False, 
                                 num_workers=num_workers, **kwargs, drop_last=True if train else False)
     n_class = len(data.classes)
+    print(f"Number of classes: {n_class}")
     return data_loader, n_class
 
 
